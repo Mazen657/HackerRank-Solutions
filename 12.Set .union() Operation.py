3ae78f9af9a9b5695f6edd1571cc5b53e0e24060
Mazen Abdallah <mazenabdallah945@gmail.com>
2026-07-19 19:08:48 +0300

    Implement union operation for English and French sets

---
+++ 12.Set .union() Operation.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+y = input()
+English = [int(y) for y in input().split()]
+x = input()
+French = [int(x) for x in input().split()]
+English = set(English)
+print(len(English.union(French)))
