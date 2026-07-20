6799bdfb77b0df2badc196d27d445c8f8dff9979
Mazen Abdallah <mazenabdallah945@gmail.com>
2026-07-20 17:45:19 +0300

    Create 22.Tuples.py

---
+++ 22.Tuples.py
@@ -0,0 +1,6 @@
+# IMPORTANT: Run this code using Python 2, not Python 3.
+if __name__ == '__main__':
+    n = int(raw_input())
+    integer_list = (map(int, raw_input().split()))
+    t = tuple(integer_list)
+    print(hash(t))
