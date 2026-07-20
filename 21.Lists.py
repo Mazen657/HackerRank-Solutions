c995ebfa2cb09b5089b84d5eba7430dbbef07ef7
Mazen Abdallah <mazenabdallah945@gmail.com>
2026-07-20 12:58:05 +0300

    Implement list operations based on user input

---
+++ 21.Lists.py
@@ -0,0 +1,20 @@
+if __name__ == '__main__':
+    l=[]
+    N = int(input())
+    for _ in range (N):
+        c = list(input().split())
+        if c[0] == "insert":
+            l.insert(int(c[1]),int(c[2]))
+        elif c[0] == "print":
+            print(l)
+        elif c[0] == "remove":
+            l.remove(int(c[1]))
+        elif c[0] == "append":
+            l.append(int(c[1]))
+        elif c[0] == "sort":
+            l.sort()
+        elif c[0] == "pop":
+            l.pop()
+        elif c[0] == "reverse":
+            l.reverse()
+    
